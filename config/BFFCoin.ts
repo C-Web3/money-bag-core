e7c04466f21c5132ad4e524733ac44cf3a9e0d48
ryanycw <ryan0213.tem04@nctu.edu.tw>
2022-12-07 23:04:31 +0800

    verify args

---
+++ config/BFFCoin.ts
@@ -0,0 +1,7 @@
+import { ethers } from "hardhat";
+
+module.exports = [
+  "BFFCoin", 
+  "BFFC", 
+  ethers.utils.parseEther('10000000')
+];
---
+++ config/BFFDistributor.ts
@@ -0,0 +1,3 @@
+module.exports = [
+  "0xade9e4cd99cF6f029d3d64fa3e21d24e47A76FEa"
+];
