11dba10463b8c73cd719f059ff79c2a16b80dde7
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-02-13 21:49:47 +0800

    adjust to non-upgradable version

--- contracts/BFFCoin.sol
+++ contracts/BFFCoin.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract BFFCoin is Initializable, ERC20Upgradeable {
-    function initialize(string memory name, string memory symbol, uint256 initialSupply) public virtual initializer {
-        __ERC20_init(name, symbol);
-        _mint(_msgSender(), initialSupply);
+contract BFFCoin is ERC20 {
+    constructor(uint256 initialSupply) ERC20("BFFCoin", "BFFC") {
+        _mint(msg.sender, initialSupply);
     }
 }
