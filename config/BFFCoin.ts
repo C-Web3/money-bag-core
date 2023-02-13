1d8ba57e7be3dbfc915305b8cbf0d1adebf8dd39
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-02-13 21:49:35 +0800

    adjust params

--- config/BFFCoin.ts
+++ config/BFFCoin.ts
@@ -1,7 +1,5 @@
 import { ethers } from "hardhat";
 
 module.exports = [
-  "BFFCoin", 
-  "BFFC", 
   ethers.utils.parseEther('10000000')
 ];
--- config/BFFDistributor.ts
+++ config/BFFDistributor.ts
@@ -1,3 +1,3 @@
 module.exports = [
-  "0xade9e4cd99cF6f029d3d64fa3e21d24e47A76FEa"
+  "0x7711F4c00B5A1Ef1E328089e021160e4765f6cF7"
 ];
